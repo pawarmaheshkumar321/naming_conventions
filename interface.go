e30758be30bcef4cb990db9d496b636ecd4edcbf
pawarmaheshkumar321 <11630290+pawarmaheshkumar321@users.noreply.github.com>
2025-06-17 00:42:02 +0530

    interface lecture

--- interface.go
+++ interface.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	r := rect{width: 10, height: 5}
+	c := circle{radius: 6}
+
+	measure(r)
+	measure(c)
+
+}
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r rect) perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perimeter())
+}
